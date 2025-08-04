669ce7ff01ed5a03f7175f51f4fac54f109f42b4
HudsonReynolds2 <hudsonre@bu.edu>
2025-08-04 17:08:55 -0400

    Able to initialize the microSD card, use it, deinitialize it, then use the HaLow modem. About to add pin mirroring from GPIO 21 to GPIO 6 so I can put a probe on GPIO 6 to see what is happening on the hidden GPIO 21

--- main/main.c
+++ main/main.c
@@ -30,6 +30,19 @@
 #include "usb/usb_helpers.h"
 #include "usb/usb_types_ch9.h"
 
+
+#include <sys/unistd.h>
+#include <sys/stat.h>
+#include "esp_vfs_fat.h"
+#include "sdmmc_cmd.h"
+
+#define PIN_SD_MISO 8
+#define PIN_SD_MOSI 9
+#define PIN_SD_CLK  7
+#define PIN_SD_CS   21  // HaLow CS is 4
+
+#define MOUNT_POINT "/sdcard"
+
 /* -------------------------- HTTP streaming -------------------------- */
 
 #define SERVER_URL          "http://192.168.137.254:8000/recordings_stream"
@@ -405,6 +418,76 @@ static void client_task(void *arg)
 
 void app_main(void)
 {
+    // Try to add MicroSD code here:
+    esp_err_t ret;
+
+    // Options for mounting the filesystem.
+    // If format_if_mount_failed is set to true, SD card will be partitioned and
+    // formatted in case when mounting fails.
+    esp_vfs_fat_sdmmc_mount_config_t mount_config = {
+        .format_if_mount_failed = false,
+        .max_files = 5,
+        .allocation_unit_size = 16 * 1024
+    };
+    sdmmc_card_t *card;
+    const char mount_point[] = MOUNT_POINT;
+    ESP_LOGI(TAG, "Initializing SD card");
+    ESP_LOGI(TAG, "Using SPI peripheral");
+    sdmmc_host_t host = SDSPI_HOST_DEFAULT();
+
+    // Force release SPI2 in case another driver grabbed it
+    spi_bus_free(host.slot);
+
+    spi_bus_config_t bus_cfg = {
+        .mosi_io_num = PIN_SD_MOSI,
+        .miso_io_num = PIN_SD_MISO,
+        .sclk_io_num = PIN_SD_CLK,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+        .max_transfer_sz = 4000,
+    };
+
+    ret = spi_bus_initialize(host.slot, &bus_cfg, SDSPI_DEFAULT_DMA);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to initialize bus.");
+        return;
+    }
+
+    // This initializes the slot without card detect (CD) and write protect (WP) signals.
+    // Modify slot_config.gpio_cd and slot_config.gpio_wp if your board has these signals.
+    sdspi_device_config_t slot_config = SDSPI_DEVICE_CONFIG_DEFAULT();
+    slot_config.gpio_cs = PIN_SD_CS;
+    slot_config.host_id = host.slot;
+
+    ESP_LOGI(TAG, "Mounting filesystem but first putting HaLow CS to High");
+    // Force HaLow module's CS high so it ignores SD card SPI traffic
+    gpio_set_direction(GPIO_NUM_4, GPIO_MODE_OUTPUT);
+    gpio_set_level(GPIO_NUM_4, 1);
+    ret = esp_vfs_fat_sdspi_mount(mount_point, &host, &slot_config, &mount_config, &card);
+
+    if (ret != ESP_OK) {
+        if (ret == ESP_FAIL) {
+            ESP_LOGE(TAG, "Failed to mount filesystem. "
+                     "If you want the card to be formatted, set the CONFIG_EXAMPLE_FORMAT_IF_MOUNT_FAILED menuconfig option.");
+        } else {
+            ESP_LOGE(TAG, "Failed to initialize the card (%s). "
+                     "Make sure SD card lines have pull-up resistors in place.", esp_err_to_name(ret));
+        }
+        return;
+    }
+    ESP_LOGI(TAG, "Filesystem mounted");
+
+    // Card has been initialized, print its properties
+    sdmmc_card_print_info(stdout, card);
+
+    // All done, unmount partition and disable SPI peripheral
+    esp_vfs_fat_sdcard_unmount(mount_point, card);
+    ESP_LOGI(TAG, "Card unmounted");
+
+    //deinitialize the bus after all devices are removed
+    spi_bus_free(host.slot);
+
+
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
     app_wlan_init();
